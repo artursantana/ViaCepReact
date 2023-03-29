b4f2bf9b7b92c5e66352611583c13d213a59b44b
artursantana <artursantana843@gmail.com>
2023-03-30 00:08:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import './Style.css';
+import api from "./Services/Api";
+
 
 function App() {
+
+  const [input,setInput] = useState('')
+  const [cep, setCep] = useState({})
+
+  const Search = async() => {
+    //01310930/json/
+
+    if(input === ''){ alert('You not typing nothing')
+  return;
+  }
+  try{
+    const response = await api.get(`${input}/json`)
+    setCep(response.data)
+    setInput('')
+    
+  }catch{
+    alert('Code not Valid!!')
+    setInput('')
+  }
+
+
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className='title'>CEP BRAZIL</h1>
+      <div className='containerInput'>
+        <input type='text'
+        placeholder='typing your code...'
+        value={input}
+        onChange={(e)=> {setInput(e.target.value)}}
+        />
+        <button className='buttonSearch' onClick={Search}>
+      <FiSearch size={15} color='#000' />
+    </button>
+        </div>
+
+        <main className="main">
+          <h2> Code: {cep.cep}</h2>
+          <span>City:{cep.localidade}</span>
+          <span>Neighborhood:{cep.bairro}</span>
+          <span>Adress: {cep.logradouro}</span>
+          <span>DDD: {cep.ddd}</span>
+        </main>
+
     </div>
   );
 }
 
-export default App;
+export default App; 
